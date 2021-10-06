34af39d8f1da6f5f72deb16e199f3bc35a2687db
Damaris Olivas <damaris.olivas@gmail.com>
2021-10-06 16:35:42 -0600

    primer commit

---
+++ src/main/java/com/example/Herramientas2/Herramientas2Application.java
@@ -0,0 +1,13 @@
+package com.example.Herramientas2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Herramientas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Herramientas2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Herramientas2/Herramientas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Herramientas2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Herramientas2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
