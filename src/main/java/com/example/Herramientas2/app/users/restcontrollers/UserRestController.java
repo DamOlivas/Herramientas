86f556ce39962d9c70a7c6654db41a481c782953
Damaris Olivas <damaris.olivas@gmail.com>
2021-10-06 18:59:10 -0600

    Se agregaron dependencias y plugins

--- src/main/java/com/example/Herramientas2/app/users/restcontrollers/UserRestController.java
+++ src/main/java/com/example/Herramientas2/app/users/restcontrollers/UserRestController.java
@@ -1,4 +1,20 @@
 package com.example.Herramientas2.app.users.restcontrollers;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserRestController {
+
+    @GetMapping("/{id}")
+    public Integer findById(@PathVariable("id")Integer id){
+        return id;
+    }
+
+    @PostMapping
+    public String create(@RequestBody String usuario){
+        return usuario;
+    }
+
 }
