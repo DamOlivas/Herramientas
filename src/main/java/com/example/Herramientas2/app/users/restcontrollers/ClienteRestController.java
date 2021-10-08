9cf85291ae97544d59e630c47cbc11afe9d4a93c
Damaris Olivas <damaris.olivas@gmail.com>
2021-10-07 22:24:36 -0600

    Se agregaron los servicios

--- src/main/java/com/example/Herramientas2/app/users/restcontrollers/ClienteRestController.java
+++ src/main/java/com/example/Herramientas2/app/users/restcontrollers/ClienteRestController.java
@@ -1,11 +1,21 @@
 package com.example.Herramientas2.app.users.restcontrollers;
 
 
+import com.example.Herramientas2.app.users.models.Cliente;
+import com.example.Herramientas2.app.users.services.ClienteService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Cliente")
 public class ClienteRestController {
+
+    private final ClienteService clienteService;
+
+    public ClienteRestController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
 
     @GetMapping("/{id}")
     public Integer findById(@PathVariable("id")Integer id){
@@ -15,6 +25,11 @@ public class ClienteRestController {
     @PostMapping
     public String create(@RequestBody String Nombre){
         return Nombre;
+    }
+
+    @GetMapping
+    public List<Cliente>findAll(){
+        return clienteService.findAll();
     }
 
 }
--- src/main/java/com/example/Herramientas2/app/users/services/ClienteService.java
+++ src/main/java/com/example/Herramientas2/app/users/services/ClienteService.java
@@ -1,4 +1,24 @@
 package com.example.Herramientas2.app.users.services;
 
+import com.example.Herramientas2.app.users.models.Cliente;
+import com.example.Herramientas2.app.users.repos.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public List<Cliente>findAll(){
+        return clienteRepository.findAll();
+    }
+
+
+
 }
